4551414a7d3693aed66d6032a07c2ef5fca6a8e7
Sonia Pupiales <soniapupiales@live.com>
2015-06-26 16:16:58 +0200

    otra
    otrass

---
+++ tiendaonlinenuevo/static/holiii/header.php
@@ -0,0 +1,11 @@
+    <!-- Header -->
+    <header>
+        <div class="container">
+            <div class="intro-text">  
+                <div class="intro-heading">Buy Online</div>              
+                <div class="intro-lead-in">Compre nuestros productos en linea...</div>
+                
+                <a href="login.php" class="page-scroll btn btn-xl">Comprar</a>
+            </div>
+        </div>
+    </header>
